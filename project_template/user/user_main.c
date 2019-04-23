7ff6b472ae8791312e62fb8e1ca446cceb093ff9
Tonix22 <emiliotonix@gmail.com>
2019-04-22 20:40:24 -0500

    RGB example

--- project_template/user/user_main.c
+++ project_template/user/user_main.c
@@ -25,6 +25,19 @@
 #include "esp_common.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "gpio.h"
+#include "espconn.h"
+
+//GPIO OUTPUT SETTINGS
+#define  LED_IO_MUX  PERIPHS_IO_MUX_MTDO_U
+
+#define  LED_IO_NUM  13
+#define  LED_IO_FUNC FUNC_GPIO13
+#define  LED_IO_PIN  GPIO_Pin_13
+
+#define RED 13
+#define GREEN 14
+#define BLUE 15
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
  * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
@@ -81,20 +94,58 @@ uint32 user_rf_cal_sector_set(void)
 *******************************************************************************/
 void Mein_Task (void *pvParameters)
 {
-	printf("enter to the task");
 	while(1)
 	{
-		vTaskDelay( 1000 );
+		vTaskDelay( 500/portTICK_RATE_MS );
 		printf("Mein Erste TASK hier\n");
 	}
 	
 }
+void RGB_LED(char r,char g, char b)
+{
+    GPIO_OUTPUT_SET(RED, r);
+    GPIO_OUTPUT_SET(GREEN, g);
+    GPIO_OUTPUT_SET(BLUE, b);
+}
+void secundaria(void *pvParameters)
+{
+    char toggle;
+	while(1)
+	{
+        RGB_LED(1,0,0);//rojo
+        printf("RED\r\n");
+		vTaskDelay( 3000/portTICK_RATE_MS );
+        RGB_LED(1,1,0);//amarillo
+        printf("Yellow\r\n");
+        vTaskDelay( 3000/portTICK_RATE_MS );
+        RGB_LED(0,1,0);//verde
+        printf("Green\r\n");
+        vTaskDelay( 3000/portTICK_RATE_MS );
+	}
+}
+void set_gpio(uint16 var)
+{
+    GPIO_ConfigTypeDef io_out_conf;
+    io_out_conf.GPIO_IntrType = GPIO_PIN_INTR_DISABLE;
+    io_out_conf.GPIO_Mode = GPIO_Mode_Output;
+    io_out_conf.GPIO_Pin = var;
+    io_out_conf.GPIO_Pullup = GPIO_PullUp_DIS;
+    gpio_config(&io_out_conf);
+}
+
 void user_init(void)
 {
     printf("SDK version:%s\n", system_get_sdk_version());
-    #if defined DEBUG
-    printf("DEFINE set on compiler")
-    #endif
-	//printf("Mein Erste Code hier\n");
+	printf("Mein Erste Code hier\n");
 	//xTaskCreate( Mein_Task, (signed char *)"Der Task", 256, NULL, 2, NULL );
+
+    set_gpio(GPIO_Pin_13);//R
+    set_gpio(GPIO_Pin_14);//G
+    set_gpio(GPIO_Pin_15);//B
+    
+
+    GPIO_OUTPUT_SET(RED, 0);
+    GPIO_OUTPUT_SET(GREEN, 0);
+    GPIO_OUTPUT_SET(BLUE, 0);
+    xTaskCreate(secundaria, (signed char *)"Task2", 256,NULL, 3,NULL);
 }
